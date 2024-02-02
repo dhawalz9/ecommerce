744268b32ff54f0adf5fc3ebcd29102a4c4ae2b7
dhawalz9 <dhawalpgupta9@gmail.com>
2024-02-02 11:23:18 +0530

    Update for hosting

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/authRoute.js"
 import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoute from "./routes/productRoutes.js"
 import cors from "cors"
+import path from "path"
 
 const app = express();
 
@@ -19,15 +20,20 @@ connectdb();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 //routes
 app.use("/api/v1/auth",authRoutes)
 app.use("/api/v1/category",categoryRoutes)
 app.use("/api/v1/product",productRoute)
 
-app.get("/",(req,res)=>{
-    res.send("<h1>Hello</h1>");
-});
+// app.get("/",(req,res)=>{
+//     res.send("<h1>Hello</h1>");
+// });
+
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on ${process.env.PORT}`)
